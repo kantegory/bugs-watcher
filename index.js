144ed4ab9553299ca5a460a7bb2aa0f620bec87b
Davyd Dobryakov <kantegory@etersoft.ru>
2020-03-25 19:33:44 +0300

    Add basic socket functions for handle socket connection on bug.html

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-server.listen(17003);
+server.listen(17003, '0.0.0.0');
+
+
+// variables
+let connections = [];
+
+
+app.get('/bugs/:id', function (request, response) {
+  let bugId = request.params.id;
+  
+  io.sockets.on('connection', function (socket) {
+    console.log(`Success connection on bug ${bugId}`);
+    connections.push({'bug': bugId, 'socket': socket});
+    console.log('connections', connections);
+
+    socket.on('disconnect', function (socket) {
+      console.log(`Disconnect on bug ${bugId}`);
+      connections.splice(connections.indexOf({'bug': bugId, 'socket': socket}), 1);
+    });
+  })
+
+  response.sendFile(__dirname + '/views/bug.html');
+})
+
 
 app.get('/bugs/:id/workers', function (request, response) {
   let bugId = request.params.id;
-  response.sendFile(__dirname + '/views/bug.html');
+  response.sendFile(__dirname + '/views/bug-workers.html');
 })
 
 
