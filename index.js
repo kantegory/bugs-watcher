0cd5a31dccbc17365298a96c07f95a8bb4282d4d
Davyd Dobryakov <kantegory@etersoft.ru>
2020-03-31 20:06:39 +0300

    Finished adding user by socket-connection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const app = require('express');
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
@@ -7,7 +7,7 @@ io.set('origins', '*:*');
 
 // variables
 let connections = [];
-let workers = [];
+let workers = {};
 
 
 io.sockets.on('connection', function (socket) {
@@ -15,9 +15,18 @@ io.sockets.on('connection', function (socket) {
   connections.push(socket);
 
   socket.on('addUser', function (data) {
-    workers.push(data);
-    console.log('New worker on bug', data);
-    io.sockets.emit('showUser', data);
+    let bugID = data.bugID;
+    let user = data.email;
+
+    if (workers[bugID] === undefined) {
+      workers[bugID] =  [];
+      workers[bugID].push(user);
+    } else if (workers[bugID].includes(user) !== true) {
+      workers[bugID].push(user);
+    }
+    io.sockets.emit('showUser', workers);
+
+    console.log('all workers', workers);
   })
 
   socket.on('disconnect', function (socket) {
